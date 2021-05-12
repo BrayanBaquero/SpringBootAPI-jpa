80de2fc870e889ab8f0ddeafab83ed83612ff9f2
BRAYAN ALIRIO BAQUERO CARDOZO <1015453029@bolivar.com>
2021-05-12 18:44:38 -0500

    ..

--- src/main/java/com/example/demo/CrudPostgresApiApplication.java
+++ src/main/java/com/example/demo/CrudPostgresApiApplication.java
@@ -3,11 +3,23 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class CrudPostgresApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudPostgresApiApplication.class, args);
 	}
+	/*
+	@Bean
+	   public WebMvcConfigurer corsConfigurer() {
+	      return new WebMvcConfigurer() {
+	         @Override
+	         public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/api").allowedOrigins("http://localhost:4200");
+	         }
+	      };
+	   }
+	   */
 
 }
